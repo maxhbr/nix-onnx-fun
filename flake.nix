049a23ff3ab7119a8432ce2883dd51ba1346f1e3
Maximilian Huber <gh@maxhbr.de>
2025-06-12 15:06:51 +0200

    add cuda libs
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
 # flake.nix
 {
   description = "Dev shell for running Phi-3 Mini ONNX on NixOS";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+      systems = [ "x86_64-linux" ];
       forAll = f: nixpkgs.lib.genAttrs systems (system:
-        f (import nixpkgs { inherit system; }));
+        f (import nixpkgs { 
+          inherit system; 
+          config.allowUnfree = true;
+          }));
     in
     {
       devShells = forAll (pkgs:
         let
-          # ←–  this is the path that really contains libstdc++.so.6
+          cudaPkgs  = pkgs.cudaPackages_12_4;     # change to _12_3, _11_8 … if needed
+          cudaLibs  = [
+            "${cudaPkgs.cudatoolkit}/lib"
+            "${cudaPkgs.cudnn}/lib"
+            "${cudaPkgs.libcublas}/lib"
+            "${cudaPkgs.cuda_cudart}/lib"
+          ];
           gccLibDir = "${pkgs.gcc.cc.lib}/lib";
           fetch-model = pkgs.writeShellScriptBin "fetch-model" ''
-            ${pkgs.huggingface-cli}/bin/huggingface-cli download \
+            ${pkgs.python313Packages.huggingface-hub}/bin/huggingface-cli download \
               microsoft/Phi-3-mini-4k-instruct-onnx \
               --include cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4/'*' \
               --local-dir phi
@@ -26,15 +34,22 @@
           default = pkgs.mkShell {
             packages = [
               pkgs.gcc                         # compiler + runtime libs
+              cudaPkgs.cudatoolkit
+              cudaPkgs.cudnn
+              cudaPkgs.libcublas
+              cudaPkgs.cuda_cudart
               pkgs.python313
               pkgs.python313Packages.venvShellHook
               pkgs.git
             ];
 
             shellHook = ''
-              export LD_LIBRARY_PATH=${gccLibDir}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+              export LD_LIBRARY_PATH=${pkgs.lib.concatStringsSep ":" (cudaLibs ++ [ gccLibDir ])}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
               if [ ! -d .venv ]; then
                 python3 -m venv .venv
+              fi
+              if [ ! -d phi ]; then
+                ${fetch-model}/bin/fetch-model
               fi
               source .venv/bin/activate
               pip install --upgrade pip
